4c6e4b8331d288da3ee250ebeaa145090750d326
mrwang90hou <mrwang90hou@163.com>
2018-09-27 18:32:19 +0800

    First Push！

---
+++ GKBusCharging/Classes/Main/View/GKTabBadgeView.h
@@ -0,0 +1,18 @@
+//
+//  GKTabBadgeView.h
+//  GKBusCharging
+//
+//  Created by mrwang90hou on 2019/9/21.
+//Copyright © 2019年 mrwang90hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKTabBadgeView : UIButton
+
+
+/** 角标数字 */
+@property (nonatomic, copy) NSString *badgeValue;
+
+
+@end
