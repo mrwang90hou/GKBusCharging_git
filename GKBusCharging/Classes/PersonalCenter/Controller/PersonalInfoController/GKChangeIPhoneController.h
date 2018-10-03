dc684d7e1242e62e1c40bcf787cd01ff98047566
mrwang90hou <mrwang90hou@163.com>
2018-10-03 22:59:00 +0800

    更换手机页面设计

---
+++ GKBusCharging/Classes/PersonalCenter/Controller/PersonalInfoController/GKChangeIPhoneController.h
@@ -0,0 +1,13 @@
+//
+//  GKChangeIPhoneController.h
+//  GKBusCharging
+//
+//  Created by 王宁 on 2018/10/2.
+//  Copyright © 2018年 goockr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKChangeIPhoneController : GKBaseSetViewController
+
+@end
--- GKBusCharging/Classes/PersonalCenter/Controller/PersonalInfoController/GKChangeIPhoneController.m
+++ GKBusCharging/Classes/PersonalCenter/Controller/PersonalInfoController/GKChangeIPhoneController.m
@@ -1,24 +1,24 @@
 //
-//  GKBindingPhoneController.m
+//  GKChangeIPhoneController.m
 //  GKBusCharging
 //
-//  Created by 王宁 on 2018/9/30.
+//  Created by 王宁 on 2018/10/2.
 //  Copyright © 2018年 goockr. All rights reserved.
 //
 
-#import "GKBindingPhoneController.h"
+#import "GKChangeIPhoneController.h"
 #import "GKSetPasswordController.h"
 
 #import "GKBindingPhoneView.h"
 
-//@interface GKBindingPhoneController ()<UINavigationControllerDelegate>
-@interface GKBindingPhoneController ()
+//@interface GKChangeIPhoneController ()<UINavigationControllerDelegate>
+@interface GKChangeIPhoneController ()
 @property(nonatomic,weak)UINavigationController*navController;
 @property (nonatomic,strong)UITextField * phoneTF;
 @property (nonatomic,strong)UITextField * codeTF;
 @end
 
-@implementation GKBindingPhoneController
+@implementation GKChangeIPhoneController
 
 //needNavBarShow;
 
@@ -42,47 +42,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    self.title = @"绑定手机";
+    self.title = @"修改手机";
     
     UIView *headerBGView = [[UIView alloc]initWithFrame:CGRectMake(0, K_HEIGHT_NAVBAR, ScreenW, ScreenH/4)];
     [headerBGView setBackgroundColor:RGBall(247)];
     [self.view addSubview:headerBGView];
     
-    UIImageView *iconConnect = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"icon_connect"]];
     
-    UIImageView *iconIphoneConnect = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon_iphone_connect"]];
     
-    UIImageView *iconConnectLogo = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"icon_connect_logo"]];
-    
-    [headerBGView addSubview:iconConnect];
-    [headerBGView addSubview:iconIphoneConnect];
-    [headerBGView addSubview:iconConnectLogo];
-    
-    [iconConnect mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.equalTo(headerBGView);
-        make.size.mas_equalTo(CGSizeMake(20, 20));
-    }];
-    [iconIphoneConnect mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(headerBGView);
-        make.left.mas_equalTo(ScreenW/4*3-60);
-        make.size.mas_equalTo(CGSizeMake(60, 60));
-    }];
-    [iconConnectLogo mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(headerBGView);
-        make.left.mas_equalTo(ScreenW/4);
-        make.size.mas_equalTo(CGSizeMake(60, 60));
+    UILabel *phoneLabel = [[UILabel alloc]init];
+    [phoneLabel setText:@"138***38000"];
+    [phoneLabel setFont:[UIFont fontWithName:PFR size:18]];
+    phoneLabel.textAlignment = NSTextAlignmentCenter;
+    [headerBGView addSubview:phoneLabel];
+    [phoneLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.mas_equalTo(headerBGView).offset(18);
+        make.centerX.equalTo(headerBGView);
+        make.size.mas_equalTo(CGSizeMake(200, 18));
     }];
     
     
-    
-    
-    
+    UILabel *titleLabel = [[UILabel alloc]init];
+    [titleLabel setText:@"当前手机号"];
+    [titleLabel setFont:[UIFont fontWithName:PFR size:16]];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    [headerBGView addSubview:titleLabel];
+    [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(headerBGView);
+        make.bottom.mas_equalTo(phoneLabel.mas_top).offset(-16);
+        make.size.mas_equalTo(CGSizeMake(140, 16));
+    }];
 //    [headerBGView mas_makeConstraints:^(MASConstraintMaker *make) {
 //        headerBGView.
 //    }];
     GKBindingPhoneView * signUpView = [[GKBindingPhoneView alloc] initWithFrame:CGRectMake(0, K_HEIGHT_NAVBAR+ScreenH/4, ScreenW, ScreenH/4*3)];
     [self.view addSubview:signUpView];
     [signUpView.nextBtn addTarget:self action:@selector(nextBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [signUpView.nextBtn setTitle:@"更换手机号" forState:UIControlStateNormal];
     self.phoneTF = signUpView.phoneTF;
     self.codeTF = signUpView.codeTF;
     
