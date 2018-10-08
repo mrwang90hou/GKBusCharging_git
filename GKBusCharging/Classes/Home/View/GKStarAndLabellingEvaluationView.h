05179fd310cfd2f1314b47837d60ece9515f2c9f
mrwang90hou <mrwang90hou@163.com>
2018-10-08 18:56:40 +0800

    GKStarAndLabellingEvaluationView【评价弹窗】第二页的设计

---
+++ GKBusCharging/Classes/Home/View/GKStarAndLabellingEvaluationView.h
@@ -0,0 +1,19 @@
+//
+//  GKStarAndLabellingEvaluationView.h
+//  STOExpressDelivery
+//
+//  Created by mrwang90hou on 2019/10/8.
+//  Copyright © 2018年 goockr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HYBStarEvaluationView.h"
+
+@interface GKStarAndLabellingEvaluationView : UIView
+@property (nonatomic,assign) Boolean starIsChanged;
+
+@property (nonatomic, assign) Boolean isCommitOrNot;
+@property (nonatomic,assign) CGFloat actualScore;
+
+@property (nonatomic,strong) HYBStarEvaluationView *starView;;
+@end
