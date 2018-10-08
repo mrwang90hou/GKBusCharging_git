52b8d04d66178380b61461c13343c20053cbff5b
mrwang90hou <mrwang90hou@163.com>
2018-10-08 18:55:32 +0800

    GKPriceEvaluationView【评价弹窗】第一个的设计

---
+++ GKBusCharging/Classes/Home/View/GKPriceEvaluationView.h
@@ -0,0 +1,14 @@
+//
+//  GKPriceEvaluationView.h
+//  STOExpressDelivery
+//
+//  Created by mrwang90hou on 2019/10/8.
+//  Copyright © 2018年 goockr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKPriceEvaluationView : UIView
+@property (nonatomic,assign) Boolean starIsChanged;
+@property (nonatomic,assign) CGFloat actualScore;
+@end
